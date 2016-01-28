30e1737ffa3dc4a1256288a3dd08041997045e52
priyalotlikar <lotlikarpriya@gmail.com>
2016-01-28 11:48:27 +0530

    This is my first introductory program

---
+++ HelloWorld/src/Hello/Hello1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Hello;
+
+/**
+ * @author dell
+ *
+ */
+public class Hello1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("helllloooo...using github for first time..yuhooooooo!!!!");
+	}
+
+}
